453f78ac3bb87dcc18f3c4fb85888e1ed77414ca
Tony Akilov <akilov.anton@rhinoda.com>
2017-08-10 13:04:16 +0600

    scrolling

--- script/script.js
+++ script/script.js
@@ -15,4 +15,16 @@ $(document).ready(function() {
 		  
 		}
 	});
+
+
+	$(document).ready(function() {
+ 
+		$('a[href^="#"]').click(function(){
+		        var el = $(this).attr('href');
+		        $('body').animate({
+		            scrollTop: $(el).offset().top}, 1500);
+		        return false; 
+		});
+		    
+	});
 });
